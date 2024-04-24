1c8524337bddd454e2b7b0b0855aacc3096dfddf
Screamer <vanagoncharov@mail.ru>
2024-04-24 13:06:49 +0300

    gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,29 @@ android {
 
 dependencies {
 
+    //coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+
+    //koin
+    val koinVersion = "3.5.6"
+    implementation ("io.insert-koin:koin-android:$koinVersion")
+    implementation("io.insert-koin:koin-android:$koinVersion")
+
+    //navigation
+    val navVersion = "2.7.7"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$navVersion")
+
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation ("androidx.activity:activity-ktx:1.9.0")
+    implementation ("androidx.fragment:fragment-ktx:1.6.2")
+
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
