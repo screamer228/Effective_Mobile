3dbe74fff8325eba9f0f685bf46f27fddb2ca7fe
Screamer <vanagoncharov@mail.ru>
2024-04-24 14:54:14 +0300

    nav_graph, bottom_nav_menu, icons and text appearance of bottom navigation view

--- app/src/main/java/com/example/effective_mobile/host/HostActivity.kt
+++ app/src/main/java/com/example/effective_mobile/host/HostActivity.kt
@@ -2,12 +2,51 @@ package com.example.effective_mobile.host
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.effective_mobile.R
+import com.example.effective_mobile.databinding.ActivityHostBinding
+import com.example.effective_mobile.presentation.HotelsFragment
+import com.example.effective_mobile.presentation.SubsFragment
+import com.example.effective_mobile.presentation.main.MainFragment
 
 class HostActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHostBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_host)
 
+        binding = ActivityHostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        prepareBottomNav()
+    }
+
+    private fun prepareBottomNav() {
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.mainFragment -> {
+                    loadFragment(MainFragment())
+                    true
+                }
+                R.id.hotelsFragment -> {
+                    loadFragment(HotelsFragment())
+                    true
+                }
+                R.id.subsFragment -> {
+                    loadFragment(SubsFragment())
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+    private fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainerView,fragment)
+        transaction.commit()
     }
 }
